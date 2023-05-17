69d4931a3224edc9a1715a8d4ede3d8ac7cc8ea5
unknown <adri.go2004@gmail.com>
2023-05-17 23:17:59 +0200

    calculadora2.java

---
+++ calculadora.java
@@ -0,0 +1,12 @@
+public class calculadora {
+    public static void main(String[] args) {
+        int resultadoSuma = calculadora.sumar(5, 3);
+        System.out.println("Suma: " + resultadoSuma);
+        
+        int resultadoResta = calculadora.restar(5, 3);
+        System.out.println("Resta: " + resultadoResta);
+        
+        int resultadoMultiplicacion = calculadora.multiplicar(5, 3);
+        System.out.println("Multiplicación: " + resultadoMultiplicacion);
+    }
+}
